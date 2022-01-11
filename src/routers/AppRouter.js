f386fe4858b19a7879de82e35d03b0b17a83fb3f
eideard-hm <ehhernandez81@misena.edu.co>
2022-01-11 09:00:59 -0500

    Added the page 404 Not Found

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AboutPage from '../pages/AboutPage'
 import HomePage from '../pages/HomePage'
+import { NotFoundPage } from '../pages/NotFoundPage'
 
 const AppRouter = () => {
     return (
@@ -8,6 +9,7 @@ const AppRouter = () => {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/about" element={<AboutPage />} />
+                <Route path="*" element={<NotFoundPage />} />
             </Routes>
         </BrowserRouter>
     )
